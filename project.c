7632c8918ad9704f0543febe317d805638eb5800
Muhammad Ihsan Abdul Hakim <75382503+ihsanahakiim@users.noreply.github.com>
2021-04-01 14:23:15 +0700

    Add files via upload

---
+++ project.c
@@ -0,0 +1,48 @@
+#include <stdio.h> //Mengimpor file stdio.h
+
+void findPos(char *dir)
+{
+    int up = 0, down = 0;
+    int left = 0, right = 0;
+    int i;
+
+    for (i = 0; dir[i] != '\0' ; i++) {
+
+        //Counts each direction
+        if (dir[i] == 'U' || dir[i] == 'u')
+            up++;
+        else if (dir[i] == 'D' || dir[i] == 'd')
+            down++;
+        else if (dir[i] == 'L' || dir[i] == 'l')
+            left++;
+        else if (dir[i] == 'R' || dir[i] == 'r')
+            right++;
+
+          //In case of illegal character in the string
+        else
+        {
+            printf("Position Unable to Find, Enter Correct Direction.");
+            break;
+        }
+    }
+
+    //Final position of robot
+    printf("Final Position of the Robot: (");
+    printf("%d", (right - left));
+    printf(",%d", (up - down));
+    printf(")");
+}
+
+int main()
+{
+    char *dir;
+
+    //Input the direction string
+    printf("Enter the Direction String:  ");
+    scanf("%s", &dir);
+
+    //Function call to calculate position
+    findPos(&dir);
+
+    return 0;
+}
