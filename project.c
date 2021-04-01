7d6bdc7c70a4bc36a45560e521b0b60546e29724
Muhammad Ihsan Abdul Hakim <75382503+ihsanahakiim@users.noreply.github.com>
2021-04-01 21:20:06 +0700

    Update project.c

--- project.c
+++ project.c
@@ -1,5 +1,17 @@
 #include <stdio.h> //Mengimpor file stdio.h
 
+//fungsi perpangkatan
+float pangkat(int i, int l){
+    if(l==0){
+        return 1;                  
+    }
+    else
+        return i * pangkat(i,l-1);  
+}
+//vektor jarak a kuadrat + b kuadrat
+int vektor(int o, int p, int q, int r){
+    return (pangkat(p-o,2) + pangkat(r-q,2));
+}
 void findPos(char *dir)
 {
     int up = 0, down = 0;
@@ -11,6 +23,15 @@ void findPos(char *dir)
     int a;
     int b;
 
+    int robothealth = 100;
+    int kecoakhealth = 100;
+    int choice;
+    
+    switch(choice){
+        case 1:
+        printf("Enter the Direction String:  ");
+        scanf("%s", &dir);    
+    
     for (i = 0; dir[i] != '\0' ; i++) {
 
         //Counts each direction
@@ -39,7 +60,10 @@ void findPos(char *dir)
     printf(",%d", (y));
     printf(")");
     
+    
+    
     /* Intializes random number generator */
+
     srand((unsigned) time(&t));
 
     /* Print 2 random numbers from 0 to 100 */
@@ -51,16 +75,38 @@ void findPos(char *dir)
    
     printf("\nJarak robot dengan kecoak");
     printf("\nJarak horizontal : %d", a-x);
-    printf("\nJarak vertikal: %d", b-y);
+    printf("\nJarak vertikal: %d", b-y
+    break;
+           
+    // The accuracy of robot firing
+           case 2:
+            if(vektor(a-x,right - left ,b-y,up - down) <= pangkat(3,2)){
+                hit += 1;
+                kecoakhealth -= 5;
+                }
+            else
+                miss += 1;
+            break;
+
+           case 3:
+           exit(1);
+           break;
+           
+    if(hit >= 20 & kecoakhealth <= 0|| robothealth> 0){
+        printf("Mission Complete\n");
+    }
+    else if (robothealth == 0 || choice == 3){
+        printf("Game Over\n");
+    }
 }
 
 int main()
 {
     char *dir;
 
-    //Input the direction string
-    printf("Enter the Direction String:  ");
-    scanf("%s", &dir);
+    //Input the direction string 
+    //printf("Enter the Direction String:  ");
+    //scanf("%s", &dir);
 
     //Function call to calculate position
     findPos(&dir);
