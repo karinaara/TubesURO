882a772da953a166b4bcdc6106201cb300375e09
Ariansyah <ariansyahedu@gmail.com>
2021-04-02 09:02:50 +0700

    Update project.c
    ada beberapa variabel yang dibikin global, bikin fungsi buat moveset kecoak, ada fungsi yang dipisah, coba cek ulang, soallnya di line 65, 163, 166 masih error

--- project.c
+++ project.c
@@ -1,6 +1,13 @@
 #include <stdio.h> //Mengimpor file stdio.h
 #include <stdlib.h> // Mengimpor file stdlib.h
 
+//global int
+int x,y;
+int a,b;
+int robothealth = 100;
+int kecoakhealth = 0;
+int count = 0;
+
 //fungsi perpangkatan
 float pangkat(int i, int l){
     if(l==0){
@@ -18,16 +25,10 @@ void findPos(char *dir)
     int up = 0, down = 0;
     int left = 0, right = 0;
     int i;
-    int x;
-    int y;
-    time_t t;
-    int a;
-    int b;
-
         
     for (i = 0; dir[i] != '\0' ; i++) {
 
-        //Counts each direction
+        //Counts each direction1
         if (dir[i] == 'U' || dir[i] == 'u')
             up++;
         else if (dir[i] == 'D' || dir[i] == 'd')
@@ -52,29 +53,28 @@ void findPos(char *dir)
     printf("%d", (x));
     printf(",%d", (y));
     printf(")");
-    
+   ;
+}
     
     
     /* Intializes random number generator */
-
+void spawn()
+{
+    time_t t;
+    kecoakhealth = 100;
     srand((unsigned) time(&t));
-
     /* Print 2 random numbers from 0 to 100 */
     a = rand() % 100;
     b = rand() % 100;
-    printf("\nPosisi kecoak adalah: ");
-    printf("(%d,", a);
+    printf("\nKecoak terdeteksi di (");
+    printf("%d,", a);
     printf("%d)", b);
    
-    printf("\nJarak robot dengan kecoak");
-    printf("\nJarak horizontal : %d", a-x);
-    printf("\nJarak vertikal: %d", b-y);
+
     
 }           
     // The accuracy of robot firing
 void Fire(int h, int v){
-    int robothealth = 100;
-    int kecoakhealth = 100;
     int hit = 0;
     int miss = 0;
 
@@ -94,38 +94,89 @@ void Fire(int h, int v){
 }
 void InactivateRobot(){
     printf("Game Over\n");
-    return 0;
+    printf("Kamu berhasil membasmi %d kecoak", count);
 }
+void KecoakMoveset(){
+    //Cek jarak
+    if (vektor(a,b)<=2)
+    //Kecoak attack
+    {
+        printf("Kecoak Attack");
+        robothealth-=2;
+        printf("Sisa HP robot %d", robothealth);
+    }
+    else{
+    //Kecoak move
+        if (a-x>1 && a-x<-1){
+            if(a<x){
+                ++a;
+            }
+            if(a>x){
+                --a;
+            }
+        }
+        if (b-y>1 && b-y<-1){
+            if(b<y){
+                ++b;
+            }
+            if(b>y){
+                --b;
+            }
+        } 
+        printf("Kecoak terbang/n Kecoak sekarang berada di koordinat (");
+        printf("%d", (a));
+        printf(",%d", (b));
+        printf(")");
+    }
+}
+void Menu(){
+    printf("\nJarak robot dengan kecoak");
+    printf("\nJarak horizontal : %d", a-x);
+    printf("\nJarak vertikal: %d", b-y);
 
-int main(){
-    char *dir;
-    int choice;
-    int x,y;
     printf("Robot Control\n");
     printf("1. Moving\n");
     printf("2. Shooting\n");
     printf("3. Inactivating\n");
     printf("\nChoose your choice: ");
-    scanf("%d", &choice);
-    //Input the direction string 
-    switch (choice)
-    {
-    case 1:
-        printf("Enter the Direction String:  ");
-        scanf("%s", &dir);
+}
 
-        //Function call to calculate position
-        findPos(&dir);
+int main(){
+    char *dir;
+    int choice;
+    spawn();
+    while (robothealth > 0);
+        if (kecoakhealth <= 0){
+            printf("Kecoak is dead\n");
+            spawn();
+            ++count;
+        }
+        
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the Direction String:  ");
+            scanf("%s", &dir);
+
+            //Function call to calculate position
+            findPos(&dir);
+            break;
+        case 2:
+            findPos(&dir);
+            //Function call to take hit and miss of shooting programm
+            Fire(x,y);
+            break;
+        case 3:
+            InactivateRobot();
+        default:
         break;
-    case 2:
-        findPos(&dir);
-        //Function call to take hit and miss of shooting programm
-        Fire(x,y);
-        break;
-    case 3:
-        InactivateRobot();
-    default:
-        break;
-    }
+        }
+        if (kecoakhealth < 20 && kecoakhealth >0 ){
+            KecoakMoveset();
+        }
+
+    printf("Robot telah meninggal\n");
+    InactivateRobot();
     return 0;
 }
